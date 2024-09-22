153f3157862a6c5b9a0e70ffff2d2655f3042e15
milind0299 <129201712+milind0299@users.noreply.github.com>
2024-09-22 22:01:03 +0530

    Update route.js

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -7,15 +7,19 @@ export async function POST(req) {
   const { name, email, message, subject, toEmail } = await req.json();
   try {
     const from = name + "<" + email + ">";
-    const { data, error } = await resend.emails.send({
-      from: "Acme <onboarding@resend.dev>",
-      to: [toEmail],
-      subject: subject,
-      react: EmailTemplate({
-        message: message,
-        from: from,
-      }),
-    });
+    const { data, error } = await fetch('https://api.resend.com/emails', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${process.env.RESEND_API_KEY}`,
+    },
+    body: JSON.stringify({
+      from: 'Acme <onboarding@resend.dev>',
+      to: ['delivered@resend.dev'],
+      subject: 'hello world',
+      html: '<strong>it works!</strong>',
+    }),
+  });
 
     if (error) {
       return Response.json({ error }, { status: 500 });
