6e5df286775dc71c9823c4ba870a48122708ecb7
milind0299 <129201712+milind0299@users.noreply.github.com>
2024-09-24 22:53:25 +0530

    links given to mobile site

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -68,19 +68,19 @@ export default function NavBar() {
                 </div>
                 <div className='py-4 flex flex-col'>
                     <ul className='uppercase'>
-                        <Link href="/">
+                        <Link href="/#main">
                             <li className='py-4 text-sm'>Home</li>
                         </Link>
-                        <Link href="/">
+                        <Link href="/#about">
                             <li className='py-4 text-sm'>About</li>
                         </Link>
-                        <Link href="/">
+                        <Link href="/#skills">
                             <li className='py-4 text-sm'>Skills</li>
                         </Link>
-                        <Link href="/">
+                        <Link href="/#projects">
                             <li className='py-4 text-sm'>Projects</li>
                         </Link>
-                        <Link href="/">
+                        <Link href="/#contact">
                             <li className='py-4 text-sm'>Contact</li>
                         </Link>
                     </ul>
